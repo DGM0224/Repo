bfc448272195f7ac4d01cdddbc86fb2db822bb54
DGM <dogun0224@naver.com>
2024-12-06 18:15:06 +0900

    1. application.properties 추가

---
+++ springbbsGM/src/main/java/com/springbootproject/bbs/SpringbbsGmApplication.java
@@ -0,0 +1,13 @@
+package com.springbootproject.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbbsGmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbbsGmApplication.class, args);
+	}
+
+}
